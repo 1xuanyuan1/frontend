f3df30049500e0aee7e1e7d2afafc31a123d9b28
戴文俊 <daiwenjun@qutoutiao.net>
2020-10-19 15:52:17 +0800

    feat: fix

---
+++ vue/08ts/ts-vue/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    {{book.name}} - {{book.anthor}}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+interface Book {
+    name: string,
+    anthor: string,
+    page: number
+}
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      book: {
+        name: '斗破苍穹',
+        anthor: '天蚕土豆'
+      } as Book
+    }
+  },
+})
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
